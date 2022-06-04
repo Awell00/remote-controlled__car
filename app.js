5d55d3b59b6eaa3a6dc07b9813f394b2e0188e23
Awell <awellpro@gmail.com>
2022-06-04 15:27:10 +0200

    clean code

--- app.js
+++ app.js
@@ -1,74 +1,62 @@
 const details__image = document.getElementById("details__image")
 const details__title = document.getElementById("details__title")
+
 const image__select__left = document.getElementById("image__select--left")
 const image__select__right = document.getElementById("image__select--right")
-const card__review__text = document.getElementById("card__review-text")
 
+const card__review__text = document.getElementById("card__review-text")
 const card__review__image = document.getElementById("card__review-image")
 const card__review__name = document.getElementById("card__review-username--name")
 const card__review__subtitle = document.getElementById("card__review-username--subtitle")
-
 const card__review__heart = document.getElementById("card__review-heart")
 
-
-
-
+// Arrow Animation of "Full Details"
 details__title.addEventListener("mouseover", function() {
-    document.getElementById("details__image").style.animation = "bounce2 2s ease infinite";
-    
+    details__image.style.animation = "bounce2 2s ease infinite" 
 } )
-
-let timer1;
-let timer2;
 
 details__title.addEventListener("mouseout", function() {
-    
     setTimeout(function() {
-        document.getElementById("details__image").style.animation = false;
-      }, 500);
+        details__image.style.animation = false
+      }, 500)
 } )
 
-let i = 1;
 
+let i = 1
+
+let timer__left, timer__right
+
+// Left Arrow Animation to Select a Comment
 function image__left() {
-
     if (i > 0) {
         i = i - 1
         image__select__left.style.setProperty("animation", "shake1 4s ease infinite", "important")
-        loadNames(i);
+        loadNames(i)
     } else {
         image__select__left.style.setProperty("animation", "hithere 1.3s ease infinite", "important")
     }
 
-    timer1 = window.setTimeout(
+    timer__left = window.setTimeout(
         () => {
-            image__select__left.style.animation = false;
-            
+            image__select__left.style.animation = false
         }, 1330);
 }
 
 image__select__left.onclick = () => {
-    timer2 = image__select__right.style.animation = false;
+    timer2 = image__select__right.style.animation = false
     clearTimeout(timer1)
     image__left()
 }
 
-function image__right() {
-    
-    if (i < 2) {
-        i = i + 1
-        image__select__right.style.setProperty("animation", "shake2 4s ease infinite", "important")
-        loadNames(i);
-    } else {
-        image__select__right.style.setProperty("animation", "hithere 1.3s ease infinite", "important")
-    }
+// Right Arrow Animation to Select a Comment
+const image__right = () => {
+    i < 2 ? ((i = i + 1) && loadNames(i)) && image__select__right.style.setProperty("animation", "shake2 4s ease infinite", "important") 
+        : (image__select__right.style.setProperty("animation", "hithere 1.3s ease infinite", "important"))
 
-    timer2 = window.setTimeout(
+    timer__right = window.setTimeout(
         () => {
             image__select__right.style.animation = false;
-            
-        }, 1330);
-    
+        }, 1330)
 }
 
 image__select__right.onclick = () => {
@@ -78,75 +66,65 @@ image__select__right.onclick = () => {
     
 }
 
-let n = 1;
 
+let heart__state = 1
+
+// Heart Animation to Like a Comment
 card__review__heart.onclick = () => {
-    
-    if (n === 1) {
-        n = 2
-        card__review__heart.src = "./image/Heart.svg"
-    } else if (n === 2) {
-        card__review__heart.src = "./image/Heart2.svg"
-        n = 1
-    }
+    heart__state === 1 ? (heart__state = 2) && (card__review__heart.src = "./image/Heart.svg") 
+        : (card__review__heart.src = "./image/Heart2.svg") && (n = 1)
     heart()
-    
 }
 
-
-function heart() {
-
-    document.getElementById("card__review-heart").style.animation = "pulse .3s infinite ease-in-out alternate";
+const heart = () => {
+    card__review__heart.style.animation = "pulse .3s infinite ease-in-out alternate"
 
     timer2 = window.setTimeout(
         () => {
-            document.getElementById("card__review-heart").style.animation = false
-      
-            
-        }, 800);
-    
+            card__review__heart.style.animation = false  
+        }, 800) 
 }
 
-
-
-
+// Access the Data in the JSON file
 async function loadNames(i) {
-    const response = await fetch('./info.json');
-    const names = await response.json();
+    const response = await fetch('./info.json')
+    const names = await response.json()
 
-    const elem = $("#card__review-text");
-    const elem1 = $("#card__review-image")
-    const elem2 = $("#card__review-username--name")
-    const elem3 = $('#card__review-username--subtitle')
-    const elem4 = $("#card__review-heart")
+    let elements = [
+        $("#card__review-text"), 
+        $("#card__review-image"), 
+        $("#card__review-username--name"), 
+        $('#card__review-username--subtitle'), 
+        $("#card__review-heart")
+    ]
 
-    function change() {
-        elem.fadeOut(function(){
-            card__review__text.innerText = names.members[i].review
-            elem.fadeIn();
-        });
+    const change = () => {
+        elements[0].fadeOut(function(){
+            card__review__text.innerText = names.members[i].text
+            elem0.fadeIn();
+        })
        
-        elem1.fadeOut(function(){
-            card__review__image.src = names.members[i].profil
+        elements[1].fadeOut(function(){
+            card__review__image.src = names.members[i].image
             elem1.fadeIn();
-        });
+        })
 
-        elem2.fadeOut(function(){
+        elements[2].fadeOut(function(){
             card__review__name.innerText = names.members[i].name
             elem2.fadeIn();
-        });
+        })
 
-        elem3.fadeOut(function(){
-            card__review__subtitle.innerText = names.members[i].profession
+        elements[3].fadeOut(function(){
+            card__review__subtitle.innerText = names.members[i].subtitle
             elem3.fadeIn();
-        });
+        })
 
-        elem4.fadeOut(function(){
-            card__review__heart.src = names.members[i].like
+        elements[4].fadeOut(function(){
+            card__review__heart.src = names.members[i].heart
             elem4.fadeIn();
-        });
+        })
     }
-    change();
+    change()
 }
   
 
